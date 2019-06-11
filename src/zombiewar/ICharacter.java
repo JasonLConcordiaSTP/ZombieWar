44275c29dd546d640d94e42b5e2f6d7d2b9abd9a
wkndevguy <southsidemike@outlook.com>
2019-06-11 18:16:56 -0500

    Initial commit. Changed methods to reflect current design. New methods are  getHealth, setHealth(int i), getAttackDamage(), and getName()

---
+++ src/zombiewar/ICharacter.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zombiewar;
+
+/**
+ *
+ * @author Mike: nelsonm25@csp.edu
+ */
+public interface ICharacter {
+    /**
+     * Returns health of current character
+     * @return this character's current health
+     */
+    public int getHealth();
+    /**
+     * Sets health of current character
+     * @param hp 
+     */    
+    public void setHealth(int hp);
+    /**
+     * Returns damage amount to deal to enemy
+     * @return current character's attackDamage
+     */
+    public int getAttackDamage();
+    /**
+     * Returns name of current character
+     * @return String of current character's name
+     */
+    public String getName();
+}
