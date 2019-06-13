e3c92fd529bfcfd10fbc984bd25881a8377a2733
PCV <PCV@DESKTOP-8V2VJ0C.local>
2019-06-12 22:14:10 -0500

    Add CommonInfected Class that extends Zombie with setName, SetHealth, and setDamage

---
+++ src/zombiewar/CommonInfected.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zombiewar;
+
+/**
+ *
+ * @author PCV
+ */
+public class CommonInfected {
+
+    public CommonInfected() {
+        setName("CommonInfected " + count++);
+        setHealth(30);
+        setDamage(5);
+    }
+}
