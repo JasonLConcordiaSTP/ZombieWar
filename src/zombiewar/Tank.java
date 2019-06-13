d32483d0a89e800e3c5e75bdea30d8447bb928eb
PCV <PCV@DESKTOP-8V2VJ0C.local>
2019-06-12 22:12:30 -0500

    Add Tank Class that extends Zombie with setName, SetHealth, and setDamage

---
+++ src/zombiewar/Tank.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zombiewar;
+
+/**
+ *
+ * @author PCV
+ */
+public class Tank extends Zombie {
+
+    public Tank() {
+        setName("Tank " + count++);
+        setHealth(150);
+        setDamage(20);
+    }
+}
