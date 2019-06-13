dd0b1db46eabb07aab471f09a9cdff173f7f095a
vlearner <barotvishal12@outlook.com>
2019-06-12 20:48:53 -0500

    First Commit

--- src/zombiewar/ZombieWar.java
+++ src/zombiewar/ZombieWar.java
@@ -16,6 +16,22 @@ public class ZombieWar {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ICharacter Soldier = new Survivor();
+        Soldier.getName();
+        Soldier.setHealth(100);
+        Soldier.setAttackDamage(10);
+        
+        ICharacter Teacher = new Survivor();
+        Teacher.getName();
+        Teacher.setHealth(50);
+        Teacher.setAttackDamage(5);
+        
+        ICharacter Child = new Survivor();
+        Child.getName();
+        Child.setHealth(20);
+        Child.setAttackDamage(2);
+        
+        
     }
     
 }
