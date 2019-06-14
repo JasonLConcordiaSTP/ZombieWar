c430b83482121effa14ad282fa62364d93e52da5
mike <mike@MIKEBOOK.in.mnice.org>
2019-06-13 19:18:07 -0500

    Created ICharacterFactory

---
+++ src/zombiewar/ICharacterFactory.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zombiewar;
+
+/**
+ *
+ * @author Mike: nelsonm25@csp.edu
+ */
+public interface ICharacterFactory {
+    public ICharacter createCharacter(String type, int... args);
+}
