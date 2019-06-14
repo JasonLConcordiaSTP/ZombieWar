f0a6f3ae0eb74d1fbbb899cab3d47df7c71922fb
vlearner <barotvishal12@outlook.com>
2019-06-14 03:26:13 -0500

    Teacher and Soldier class
    Teacher and Soldier class

---
+++ src/zombiewar/Soldier.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zombiewar;
+
+/**
+ *
+ * @author vishalbarot
+ */
+public class Soldier extends Survivor {
+    public static int count = 0;
+    
+    public Teacher(){
+        setName("Soldier " + count++);
+        setHealth(100);
+        setDamage(10);
+    }
+}
