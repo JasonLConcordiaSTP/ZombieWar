0c16168b15d8c7886146dfc530cd630b430f588a
vlearner <barotvishal12@outlook.com>
2019-06-15 14:48:29 -0500

    Zombie class added

---
+++ src/zombiewar/Zombie.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zombiewar;
+
+/**
+ *
+ * @author vishalbarot
+ */
+
+public abstract class Zombie {
+    private int health;
+    private int damage;
+    private String name;
+    
+    public Zombie() {
+
+    }
+    
+    public int getHealth() {
+        return health;
+    }
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    public int getDamage() {
+        return damage;
+    }
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
