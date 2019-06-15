eec09f598984a1dfc95175e07ce9041de89db754
vlearner <barotvishal12@outlook.com>
2019-06-15 14:48:48 -0500

    Survivor class added

---
+++ src/zombiewar/Survivor.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zombiewar;
+
+/**
+ *
+ * @author vishalbarot
+ */
+public abstract class Survivor {
+     private int health;
+     private int damage;
+     private String name;
+
+     public Survivor() {
+
+     }
+
+     public int getHealth() {
+         return health;
+     }
+     public void setHealth(int health) {
+         this.health = health;
+     }
+     public int getDamage() {
+         return damage;
+     }
+     public void setDamage(int damage) {
+         this.damage = damage;
+     }
+     public String getName() {
+         return name;
+     }
+     public void setName(String name) {
+         this.name = name;
+     }
+ }
