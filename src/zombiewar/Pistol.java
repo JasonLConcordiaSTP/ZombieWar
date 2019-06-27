3fd2692fd49d0c1bfa0df95286ec5d8a46155eaa
Myra Ausherman <mausherm@gmail.com>
2019-06-27 18:58:04 -0500

    Added weapon class 'Pistol'.

---
+++ src/zombiewar/Pistol.java
@@ -0,0 +1,20 @@
+/*
+ * CSC422 - Summer 2019 
+ * Zombie War Group Project
+ * Authors: Mike Nelson, Myra Ausherman, Peter Vang, Vishal Barot, Jason Lundquist.
+*/
+package zombiewar;
+
+/**
+ *
+ * @author mausherm
+ */
+public class Pistol extends Weapon {
+    //constructor
+    public Pistol() {
+        this.accuracy = 35;
+        this.damage = 40;
+    }
+}
+
+
