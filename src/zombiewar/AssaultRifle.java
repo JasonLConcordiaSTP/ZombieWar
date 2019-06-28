84abed01020fb2700749fb2e0030d46fb33a7bb1
lundqja <lundqja@USNB170412U0AXE.GLOBAL.ECOLAB.CORP>
2019-06-28 07:27:59 -0500

    Attempting to fix merge conflicts and project property settings.

--- src/zombiewar/AssaultRifle.java
+++ src/zombiewar/AssaultRifle.java
@@ -2,7 +2,6 @@
  * CSC422 - Summer 2019 
  * Zombie War Group Project
  * Authors: Mike Nelson, Myra Ausherman, Peter Vang, Vishal Barot, Jason Lundquist.
- * 
  */
 package zombiewar;
 
@@ -10,14 +9,12 @@ package zombiewar;
  *
  * @author vishalbarot
  */
-public class Child extends Survivor {
-    public static int count = 0;
+public class AssaultRifle extends Weapon {
     
-    public Child(Weapon w) {
-        setName("Child " + count++);
-        setHealth(20);
-        setDamage(2);
-        this.weapon = w;
+    public AssaultRifle (){
+        
+         this.accuracy = 90;
+         this.damage = 100;
     }
-
+    
 }
---
+++ src/zombiewar/Crowbar.java
@@ -0,0 +1,20 @@
+/*
+ * CSC422 - Summer 2019 
+ * Zombie War Group Project
+ * Authors: Mike Nelson, Myra Ausherman, Peter Vang, Vishal Barot, Jason Lundquist.
+ */
+package zombiewar;
+
+/**
+ *
+ * @author PCV
+ */
+public class Crowbar extends Weapon {
+    
+     // FryingPan Constructor
+    public Crowbar() {
+        this.accuracy = 80;
+        this.damage = 10;
+    }
+    
+}
